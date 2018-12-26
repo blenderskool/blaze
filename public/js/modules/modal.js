f5b35aef9cba4b84cf42316335d6c780f4e1902c
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-12-26 21:25:04 +0530

    Improved accessibility

--- public/js/modules/modal.js
+++ public/js/modules/modal.js
@@ -1,41 +1,53 @@
 class Modal {
 
   constructor(child) {
-    const body = document.body;
-
+    /**
+     * Updates the exisiting modal container
+     */
     if (document.getElementById('mdl')) {
       const container = document.getElementById('mdl');
       clearNode(container);
 
-      const modal = document.createElement('div');
-      modal.classList.add('modal');
 
-      const close = document.createElement('span');
-      close.classList.add('icon-add');
-      close.addEventListener('click', () => Modal.close());
-
-      modal.appendChild(child);
-      container.appendChild(close);
-      container.appendChild(modal);
+      Modal.addControllers(container, child);
     }
+    /**
+     * Create a new modal container
+     */
     else {
       const container = document.createElement('div');
       container.id = 'mdl';
 
-      const modal = document.createElement('div');
-      modal.classList.add('modal');
-
-      const close = document.createElement('span');
-      close.classList.add('icon-add');
-      close.addEventListener('click', () => Modal.close());
-
-      modal.appendChild(child);
-      container.appendChild(close);
-      container.appendChild(modal);
-      body.appendChild(container);
+      document.body.appendChild(Modal.addControllers(container, child));
     }
   }
 
+  /**
+   * Adds additional controls to the modal container
+   */
+  static addControllers(container, child) {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    const close = document.createElement('span');
+    close.classList.add('icon-add');
+    close.setAttribute('role', 'button');
+    close.tabIndex = 0;
+    close.setAttribute('aria-label', 'Close Modal');
+    close.addEventListener('click', () => Modal.close());
+    close.addEventListener('keydown', e => { if (e.which === 13) Modal.close(); });
+
+    modal.appendChild(child);
+
+    container.appendChild(close);
+    container.appendChild(modal);
+
+    return container;
+  }
+
+  /**
+   * Open the modal window
+   */
   static open() {
     const container = document.getElementById('mdl');
     document.getElementById('app').style.filter = 'blur(8px)';
@@ -44,6 +56,9 @@ class Modal {
 
   }
 
+  /**
+   * Close the modal window
+   */
   static close() {
     const container = document.getElementById('mdl');
 
