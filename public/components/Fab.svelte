1f0f9619d1fc7c6ed9c663fb6abf05967f4bbf7e
Adam <adam.kravec@student.tuke.sk>
2019-11-14 17:37:18 +0100

    code and design improvements

--- public/components/Fab.svelte
+++ public/components/Fab.svelte
@@ -2,13 +2,12 @@
 	import { createEventDispatcher } from 'svelte';
 
 	const dispatch = createEventDispatcher();
-  export let text, id, icon, disabled = false;
+  export let text, icon, disabled = false;
 </script>
 
 
 <button
   class="fab"
-  id={id}
   aria-label={text}
   on:click={e => dispatch('click', e)}
   disabled={disabled}
