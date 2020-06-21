93ba77be30972eecc6e308f48c3a58446b3eb630
blenderskool <akash.hamirwasia@gmail.com>
2020-06-21 16:58:10 +0530

    Add WebTorrent support with WebSockets as fallback

---
+++ src/utils/fileShare.js
@@ -0,0 +1,201 @@
+import constants from '../../constants';
+
+const trackers = {
+  announce: ['wss://tracker.btorrent.xyz', 'wss://tracker.openwebtorrent.com'],
+};
+
+class FileShare {
+
+  constructor(socket, torrentOpts) {
+    this.socket = socket;
+    this.torrentClient = new WebTorrent({
+      tracker: {
+        ...trackers,
+        rtcConfig: {
+          iceServers: [
+            {
+              'urls': ['stun:stun.l.google.com:19305', 'stun:stun1.l.google.com:19305']
+            }
+          ]        
+        }
+      }
+    });
+  }
+
+  get isWebRTC() {
+    return WebTorrent.WEBRTC_SUPPORT;
+  }
+
+  receiveFiles({ onMeta, onProgress, onDone }) {
+    let metaData = {};
+
+    this.socket.listen(constants.FILE_TORRENT, ({ infoHash, ...data }) => {
+      if (onMeta) {
+        metaData = data;
+        onMeta(data);
+      }
+
+      this.torrentClient.add(infoHash, trackers, torrent => {
+        this._onTorrent({ torrent, onProgress, onDone });
+      });
+    });
+
+    let fileParts = [];
+    let size = 0, statProg = 0.25;
+    this.socket.listen(constants.FILE_INIT, (data) => {
+      if (data.end) {
+        // TODO: build the file
+        if (fileParts.length) {
+          callbacks.done(new Blob(fileParts));
+          fileParts = [];
+          size = 0;
+          statProg = 0.25;
+        }
+      }
+      else {
+        metaData = data;
+        callbacks.meta(data);
+      }
+    });
+
+    this.socket.listen(constants.CHUNK, data => {
+      fileParts.push(data);
+      size += data.byteLength;
+
+      const progress = size / metaData.size;
+
+      callbacks.progress({ progress });
+
+      if (progress >= statProg) {
+        statProg += 0.15;
+        this.socket.send(constants.FILE_STATUS, {
+          progress: statProg,
+          peer: this.socket.name,
+        });
+      }
+    });
+  }
+
+  _onTorrent({ torrent, onProgress, onDone }) {
+    let updateInterval;
+
+    const update = () => {
+      onProgress(torrent);
+
+      if (!updateInterval) {
+        updateInterval = setInterval(update, 500);
+      }
+
+      if (!torrent.uploadSpeed && !torrent.downloadSpeed) {
+        onDone();
+        torrent.destroy();
+        clearInterval(updateInterval);
+        updateInterval = undefined;
+      }
+    }
+
+    torrent.on('upload', update);
+    torrent.on('download', update);
+    torrent.on('done', () => {
+      onDone(torrent.files);
+    });
+  }
+
+  sendFileSocket({ file, numPeers, onMeta, onSocketProgress }) {
+    const reader = file.stream().getReader();
+    const transferStatus = {
+      peers: Array(numPeers - 1),
+      progress: 0.25,
+    };
+    let sharedSize = 0, progress = 0;
+
+    const meta = [{
+      name: file.name,
+      size: file.size,
+    }];
+
+    onMeta(meta);
+    
+    this.socket.send(constants.FILE_INIT, {
+      user: this.socket.name,
+      size: file.size,
+      meta,
+    });
+
+    return new Promise((resolve, reject) => {
+
+      const stream = async () => {
+        const { done, value } = await reader.read();
+        if (done) {
+          this.socket.off(constants.FILE_STATUS);
+          this.socket.send(constants.FILE_INIT, {
+            end: true,
+          });
+          resolve();
+          return;
+        }
+
+        this.socket.send(constants.CHUNK, value.buffer);
+        sharedSize += value.byteLength;
+        progress = sharedSize / file.size;
+
+        onSocketProgress({ progress });
+
+        if (transferStatus.peers.length === numPeers - 1 && progress < transferStatus.progress) {
+          setTimeout(stream, 1);
+        }
+      };
+
+      this.socket.listen(constants.FILE_STATUS, ({ peer, progress }) => {
+        if (progress !== transferStatus.progress) {
+          transferStatus.progress = progress;
+          transferStatus.peers = [ peer ];
+        } else {
+          transferStatus.peers.push(peer);
+        }
+        
+        stream();
+      });
+
+      stream();
+    });
+  }
+
+  async sendFiles({ numPeers, input, useTorrent, onMeta, onSocketProgress, onTorrentProgress, onDone }) {
+    if (useTorrent) {
+      this.torrentClient.seed(input, trackers, torrent => {
+        this._onTorrent({
+          torrent,
+          onProgress: onTorrentProgress,
+          onDone,
+        });
+
+        const filesMeta = torrent.files.map(file => ({
+          name: file.name,
+          size: file.length,
+        }));
+
+        onMeta(filesMeta);
+        this.socket.send(constants.FILE_TORRENT, {
+          infoHash: torrent.infoHash,
+          user: this.socket.name,
+          size: torrent.length,
+          meta: filesMeta,
+        });
+
+      });
+    }
+    else {
+      if (!input) return;
+      input = input && (input.length ? [...input] : [ input ]);
+
+      for(const file of input) {
+        await this.sendFileSocket({ file, numPeers, onMeta, onSocketProgress });
+      }
+
+    }
+  }
+
+}
+
+export default FileShare;
---
+++ src/utils/socketConnect.js
@@ -0,0 +1,26 @@
+import Socket from '../../utils/socket';
+import constants from '../../constants';
+import FileShare from './fileShare';
+
+/**
+ * Opens a socket connection to join a room
+ * @param {String} room Room to join
+ * @param {String} username Name of the user joining the room
+ */
+function socketConnect(room, username) {
+  const socket = new Socket(new WebSocket(process.env.WS_HOST));
+  const fileShare = new FileShare(socket);
+  socket.name = username;
+  
+  socket.on('open', () => {
+    socket.send(constants.JOIN, {
+      roomName: room,
+      name: username,
+      peerId: fileShare.isWebRTC ? fileShare.torrentClient.peerId : null,
+    });
+  });
+
+  return fileShare;
+}
+
+export default socketConnect;
