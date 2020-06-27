f5b9a0c313dd8f0295adfe7bef89b6f435128b8e
blenderskool <akash.hamirwasia@gmail.com>
2020-06-27 22:24:54 +0530

    Refactor formatSize function

--- src/utils/formatSize.js
+++ src/utils/formatSize.js
@@ -1,10 +1,14 @@
 /**
  * Returns an easy to read formatted size
  * @param {Number} size Size of the file in bytes
+ * @param {Number} precision Number of digits after decimal
  * @returns {String} Formatted size
  */
-function formatSize(size) {
-  return size / (1024 * 1024) < 1 ? Math.round((size / 1024) * 10) / 10 + 'KB' : Math.round((size / (1024 * 1024) * 10)) / 10 + 'MB';
+function formatSize(size, precision = 1) {
+  const kbs = size / 1024;
+  const mbs = kbs / 1024;
+
+  return mbs < 1 ? `${kbs.toFixed(precision)}KB` : `${mbs.toFixed(precision)}MB`;
 }
 
 export default formatSize;
