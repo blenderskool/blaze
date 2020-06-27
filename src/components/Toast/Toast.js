be55ef651ea20c8ad83eb539316c71bb822eb7d5
blenderskool <akash.hamirwasia@gmail.com>
2020-06-27 13:27:41 +0530

    Add Toast component

---
+++ src/components/Toast/Toast.js
@@ -0,0 +1,9 @@
+import { h } from 'preact';
+
+import './Toast.scss';
+
+const Toast = props => (
+  <div class="toast" {...props} />
+);
+
+export default Toast;
---
+++ src/components/Toast/index.js
@@ -0,0 +1,36 @@
+import { h, render } from 'preact';
+
+import Toast from './Toast';
+import './ToastContainer.scss';
+
+let container;
+let queue = [];
+
+function ToastContainer() {
+  return (
+    <div class="toast-container">
+      {queue.map(message => <Toast>{message}</Toast>)}
+    </div>
+  );
+}
+
+const setQueue = (newQueue) => {
+  queue = newQueue;
+
+  if (!container) {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  }
+
+  render(<ToastContainer />, container);
+};
+
+const toast = (message) => {
+  setQueue([ ...queue, message ]);
+
+  // setTimeout(() => {
+  //   setQueue(queue.slice(0, -1));
+  // }, 4000);
+}
+
+export { toast };
