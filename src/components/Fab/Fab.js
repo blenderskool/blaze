52a802a20b1c17cc981459400dea4717def6450c
blenderskool <akash.hamirwasia@gmail.com>
2020-08-09 20:22:20 +0530

    Add variant prop to FAB for supported variants
    - auto: Large FAB on large mobiles, tablets and desktops and small
    FAB on small mobiles
    - lg: Large FAB
    - sm: Small FAB

--- src/components/Fab/Fab.js
+++ src/components/Fab/Fab.js
@@ -1,16 +1,27 @@
 import { h } from 'preact';
+import PropTypes from 'prop-types';
 
 import './Fab.scss';
 
-function Fab({ children, text, ...props }) {
+function Fab({ children, text, variant, ...props }) {
 
   return (
-    <button class="fab" ariaLabel={text} {...props}>
+    <button class={`fab ${variant}`} ariaLabel={text} {...props}>
       {children}
       <div class="lg-text">{text}</div>
       <slot />
     </button>
   );
 }
+
+Fab.propTypes = {
+  variant: PropTypes.oneOf(['sm', 'lg', 'auto']),
+  text: PropTypes.string,
+};
+
+Fab.defaultProps = {
+  variant: 'auto',
+  text: '',
+};
 
 export default Fab;
