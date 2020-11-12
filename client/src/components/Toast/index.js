6988a5ef5a2d8675455bb0284aaed903b63b244f
blenderskool <akash.hamirwasia@gmail.com>
2020-11-12 11:51:12 +0530

    Add toast slide animation

--- client/src/components/Toast/index.js
+++ client/src/components/Toast/index.js
@@ -1,18 +1,9 @@
 import { h, render } from 'preact';
-
-import Toast from './Toast';
-import './ToastContainer.scss';
+import { nanoid } from 'nanoid';
+import ToastContainer from './ToastContainer';
 
 let container;
 let queue = [];
-
-function ToastContainer() {
-  return (
-    <div class="toast-container">
-      {queue.map(message => <Toast>{message}</Toast>)}
-    </div>
-  );
-}
 
 const setQueue = (newQueue) => {
   queue = newQueue;
@@ -22,15 +13,22 @@ const setQueue = (newQueue) => {
     document.body.appendChild(container);
   }
 
-  render(<ToastContainer />, container);
+  render(<ToastContainer queue={queue} />, container);
 };
 
-const toast = (message) => {
-  setQueue([ ...queue, message ]);
+setQueue([]);
 
-  setTimeout(() => {
-    setQueue(queue.slice(0, -1));
-  }, 4000);
-}
+const toast = (message) => {
+  const item = { message, id: nanoid(8) };
+  if (queue.length < 4) {
+    setQueue([ ...queue, item ]);
+
+    setTimeout(() => {
+      setQueue(queue.slice(1));
+    }, 4000);
+  } else {
+    setQueue([ ...queue.slice(1), item ])
+  }
+};
 
 export { toast };
