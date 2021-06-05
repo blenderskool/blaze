0854cffefa5ad0059345f074e399c8ab698c00b4
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2021-06-05 22:53:34 +0530

    Update URL to consume from env variable

--- api/badges/status.js
+++ api/badges/status.js
@@ -11,7 +11,7 @@ export default async (req, res) => {
   res.setHeader('Cache-control', 'max-age=0, s-maxage=60');
 
   try {
-    await fetch('https://blaze.beaconapp.in');
+    await fetch(process.env.SERVER_HOST);
     res.send(render(<Badge type="success" width={90} height={24} title="status" value="up" />));
   } catch(err) {
     res.send(render(<Badge type="error" width={120} height={24} title="status" value="down" />));
