6eeda2da6491251a4da323863b7eb464ca252eda
blenderskool <akash.hamirwasia@gmail.com>
2021-06-23 20:00:24 +0530

    Add AppLanding layout

---
+++ client/src/routes/App/layouts/AppLanding/AppLanding.js
@@ -0,0 +1,76 @@
+import { h } from 'preact';
+import { ArrowDownCircle, Gift, Grid, PieChart, Settings } from 'preact-feather';
+import { Link } from 'preact-router/match';
+import { useContext } from 'preact/hooks';
+import { PWAInstall } from '../../contexts/PWAInstall';
+
+import './AppLanding.scoped.scss';
+
+const Navigation = () => (
+  <>
+    <Link href="/app/" class="nav-item" activeClassName="active">
+      <Grid />
+      <span class="label">Rooms</span>
+    </Link>
+    <Link href="/app/activity" class="nav-item" activeClassName="active">
+      <PieChart />
+      <span class="label">Activity</span>
+    </Link>
+    <Link href="/app/settings" class="nav-item" activeClassName="active">
+      <Settings />
+      <span class="label">Settings</span>
+    </Link>
+    <Link href="/app/support" class="nav-item" activeClassName="active">
+      <Gift />
+      <span class="label">Support</span>
+    </Link>
+  </>
+);
+
+function AppLanding({ children, title, subtitle }) {
+  const { installable, deferredPrompt, setInstallable } = useContext(PWAInstall);
+
+  const handleInstall = async () => {
+    if (typeof window === 'undefined' || !installable || deferredPrompt === null) return;
+
+    setInstallable(false);
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+
+    window.ga('send', 'event', {
+      eventCategory: 'pwa-install',
+      eventAction: 'promo-shown',
+      outcome,
+    });
+  };
+
+  return (
+    <div>
+      <header class="app-header">
+        <div class="title-nav-wrapper">
+          <h1 class="title">{title}</h1>
+          <div class="nav">
+            {
+              installable && (
+                <button class="nav-item install" onClick={handleInstall}>
+                  <ArrowDownCircle />
+                  <span class="label">Install</span>
+                </button>
+              )
+            }
+            <Navigation />
+          </div>
+        </div>
+        <p class="subtitle">{subtitle}</p>
+      </header>
+
+      {children}
+
+      <div class="tab-links">
+        <Navigation />
+      </div>
+    </div>
+  );
+};
+
+export default AppLanding;
