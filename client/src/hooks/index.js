0e3650b14e067e0a685480432c050b37911d495e
blenderskool <akash.hamirwasia@gmail.com>
2021-06-30 14:00:11 +0530

    Replace localStorage hooks with react-localstorage-hooks package
    react-localstorage-hooks is a package that is extracted from the
    hooks designed in this project. The package adds additional hooks
    with improved API and is therefore being used

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -2,4 +2,3 @@ export {default as useOnline} from './useOnline';
 export {default as useSWMessage} from './useSWMessage';
 export {default as useOnHistoryPush} from './useOnHistoryPush';
 export {default as useInstantRoom} from './useInstantRoom';
-export {default as useLocalStorageSelector} from './useLocalStorageSelector';
