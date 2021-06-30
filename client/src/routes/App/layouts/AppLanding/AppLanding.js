601a9d3832338ec25e880fd80ebe58b225038191
blenderskool <akash.hamirwasia@gmail.com>
2021-06-30 14:18:51 +0530

    Add overflow ellipsis to title

--- client/src/routes/App/layouts/AppLanding/AppLanding.js
+++ client/src/routes/App/layouts/AppLanding/AppLanding.js
@@ -1,4 +1,5 @@
 import { h } from 'preact';
+import { memo } from 'preact/compat';
 import { ArrowDownCircle, Gift, Grid, PieChart, Settings } from 'preact-feather';
 import { Link } from 'preact-router/match';
 import { useContext } from 'preact/hooks';
@@ -6,26 +7,28 @@ import { PWAInstall } from '../../contexts/PWAInstall';
 
 import './AppLanding.scoped.scss';
 
-const Navigation = () => (
-  <>
-    <Link href="/app/" class="nav-item" activeClassName="active">
-      <Grid />
-      <span class="label">Rooms</span>
-    </Link>
-    <Link href="/app/activity" class="nav-item" activeClassName="active">
-      <PieChart />
-      <span class="label">Activity</span>
-    </Link>
-    <Link href="/app/settings" class="nav-item" activeClassName="active">
-      <Settings />
-      <span class="label">Settings</span>
-    </Link>
-    <Link href="/app/support" class="nav-item" activeClassName="active">
-      <Gift />
-      <span class="label">Support</span>
-    </Link>
-  </>
-);
+const Navigation = memo(function Navigation() {
+  return (
+    <>
+      <Link href="/app/" class="nav-item" activeClassName="active">
+        <Grid />
+        <span class="label">Rooms</span>
+      </Link>
+      <Link href="/app/activity" class="nav-item" activeClassName="active">
+        <PieChart />
+        <span class="label">Activity</span>
+      </Link>
+      <Link href="/app/settings" class="nav-item" activeClassName="active">
+        <Settings />
+        <span class="label">Settings</span>
+      </Link>
+      <Link href="/app/support" class="nav-item" activeClassName="active">
+        <Gift />
+        <span class="label">Support</span>
+      </Link>
+    </>
+  )
+});
 
 function AppLanding({ children, title, subtitle }) {
   const { installable, deferredPrompt, setInstallable } = useContext(PWAInstall);
@@ -52,7 +55,7 @@ function AppLanding({ children, title, subtitle }) {
           <div class="nav">
             {
               installable && (
-                <button class="nav-item install" onClick={handleInstall}>
+                <button class="nav-item install" title="Install Blaze" onClick={handleInstall}>
                   <ArrowDownCircle />
                   <span class="label">Install</span>
                 </button>
