d901f29a85c9b4ebf046d2182b559d086b3b7748
av4 <av4@cormea.com>
2022-09-17 14:19:20 -0600

    minimal footer

--- client/src/routes/Pages/components/Footer/Footer.js
+++ client/src/routes/Pages/components/Footer/Footer.js
@@ -5,26 +5,11 @@ import './Footer.scss';
 const Footer = () => (
   <footer>
     <div>
-      <a href="https://bit.ly/36uX8oU" aria-label="DigitalOcean" target="_blank" rel="noreferrer">
-        <img class="digital-ocean" src="https://opensource.nyc3.cdn.digitaloceanspaces.com/attribution/assets/PoweredByDO/DO_Powered_by_Badge_white.svg" alt="Powered by DigitalOcean" />
-      </a>
-    </div>
-
-    <div>
-      <span>
-        © 2018 - present&nbsp;
-        <a href="https://akashhamirwasia.com" target="_blank" rel="noreferrer">
-          Akash Hamirwasia
-        </a>
-        .&nbsp;
+      <span style="font-weight:100">
+        ©2022 - present&nbsp;
+          <a href="//github.com/av4/blaze">Blazr</a> - A free share now adaptation of <a href="//blaze.vercel.com" target="_blank">Blaze</a><br>
+          <sup><a href="//github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank" style="color:#888;font-weight:100">MIT License</a>
       </span>
-      <span>
-        Released under the
-        <a href="https://github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank" rel="noreferrer"> MIT License</a>
-      </span>
-      <div>
-        Designed & Developed in 🇮🇳
-      </div>
     </div>
   </footer>
 );
