130436940d0fadafd568b77faa1949d6ba697149
av4 <av4@cormea.com>
2022-09-17 14:23:48 -0600

    nix span

--- client/src/routes/Pages/components/Footer/Footer.js
+++ client/src/routes/Pages/components/Footer/Footer.js
@@ -4,14 +4,13 @@ import './Footer.scss';
 
 const Footer = () => (
   <footer>
-    <div>
-      <span style="font-weight:100">
+    <div style="font-weight:100">
         ©2022 - present&nbsp;
-          <a href="//github.com/av4/blaze">Blazr</a> - A free share now adaptation of <a href="//blaze.vercel.com" target="_blank">Blaze</a><br>
-          <sup>
-            <a href="//github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank" style="color:#888;font-weight:100">MIT License</a>
-          </sup>
-      </span>
+        <a href="//github.com/av4/blaze">Blazr</a> - A free share now adaptation of <a href="//blaze.vercel.com" target="_blank">Blaze</a>
+        <br>
+        <sup>
+          <a href="//github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank" style="color:#888;font-weight:100">MIT License</a>
+        </sup>
     </div>
   </footer>
 );
