e3de68eedcfa888fd0c1b4b8db7fec56d83dcbd6
av4 <av4@cormea.com>
2022-09-17 21:36:28 -0600

    dash cleanup

--- client/src/routes/Pages/components/Footer/Footer.js
+++ client/src/routes/Pages/components/Footer/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => (
     <div style="font-weight:100">
         <a href="//github.com/av4/blaze">Blazr</a> ©<span id="cpyear">2022</span> - <a href="//github.com/av4/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
         <br />
-        <span style="font-size:smaller">A free share now adaptation of <a href="//blaze.vercel.com" target="_blank">Blaze</a> 
-        - <a href="//github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
+        <span style="font-size:smaller">
+          A free share now adaptation of <a href="//blaze.vercel.com" target="_blank">Blaze</a> - <a href="//github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
         </span>
     </div>
     <script>
