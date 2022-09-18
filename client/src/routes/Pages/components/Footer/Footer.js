612b95eb28fc535b302bad395a5ded59756d1712
av4 <av4@cormea.com>
2022-09-17 21:38:10 -0600

    .sec-clr

--- client/src/routes/Pages/components/Footer/Footer.js
+++ client/src/routes/Pages/components/Footer/Footer.js
@@ -5,10 +5,10 @@ import './Footer.scss';
 const Footer = () => (
   <footer>
     <div style="font-weight:100">
-        <a href="//github.com/av4/blaze">Blazr</a> ©<span id="cpyear">2022</span> - <a href="//github.com/av4/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
+        <a class="sec-clr" href="//github.com/av4/blaze">Blazr</a> ©<span id="cpyear">2022</span> - <a class="sec-clr" href="//github.com/av4/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
         <br />
         <span style="font-size:smaller">
-          A free share now adaptation of <a href="//blaze.vercel.com" target="_blank">Blaze</a> - <a href="//github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
+          A free share now adaptation of <a class="sec-clr" href="//blaze.vercel.com" target="_blank">Blaze</a> - <a class="sec-clr" href="//github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
         </span>
     </div>
     <script>
