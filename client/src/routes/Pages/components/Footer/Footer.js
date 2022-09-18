0da9bedba32da55e8cc37a2c7ba88620719f88e0
av4 <av4@cormea.com>
2022-09-17 20:41:33 -0600

    auto copyright year

--- client/src/routes/Pages/components/Footer/Footer.js
+++ client/src/routes/Pages/components/Footer/Footer.js
@@ -5,13 +5,15 @@ import './Footer.scss';
 const Footer = () => (
   <footer>
     <div style="font-weight:100">
-        ©2022 - present&nbsp;
-        <a href="//github.com/av4/blaze">Blazr</a> - <a href="//github.com/av4/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
+        <a href="//github.com/av4/blaze">Blazr</a> ©<span id="cpyear">2022</span> - <a href="//github.com/av4/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
         <br />
         <span style="font-size:smaller">A free share now adaptation of <a href="//blaze.vercel.com" target="_blank">Blaze</a> 
         - <a href="//github.com/blenderskool/blaze/blob/master/LICENSE" target="_blank">MIT License</a>
         </span>
     </div>
+    <script>
+      document.getElementById('cpyear').innerText = ('2022 - ' + (new Date).getFullYear()).replace('2022 - 2022', '2022');
+    </script>
   </footer>
 );
 
