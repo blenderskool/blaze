fe3c5948e0c503b9bd555cf32e3ac20e17d4c59d
av4 <av4@cormea.com>
2022-09-18 17:39:30 -0600

    resetAll 11

--- client/src/routes/App/Rooms/Rooms.js
+++ client/src/routes/App/Rooms/Rooms.js
@@ -145,7 +145,7 @@ function Rooms({ isOnline }) {
                         e = globalThis;
                         e.localStorage.clear();
                         useState('');
-                        route('/', true);
+                        route('/done');
                       }}>Clear everything (user &amp; rooms)</a>
                     </div>
                   </>
