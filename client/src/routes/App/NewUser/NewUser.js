2a32ff34cbf171d76cf327008e4a4a01d1e85bb8
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2022-12-20 11:14:31 +0000

    Update new user page design

--- client/src/routes/App/NewUser/NewUser.js
+++ client/src/routes/App/NewUser/NewUser.js
@@ -1,6 +1,10 @@
 import { h } from 'preact';
 import { createLocalStorageDispatch } from 'react-localstorage-hooks';
 import NicknameInput from '../../../components/NicknameInput/NicknameInput';
+import network from '../../../assets/images/illustrations/network.svg';
+import logo from '../../../assets/images/logo.svg';
+
+import './NewUser.scoped.scss';
 
 const registerUser = createLocalStorageDispatch('blaze', (state, e) => {
   e.preventDefault();
@@ -16,8 +20,24 @@ const registerUser = createLocalStorageDispatch('blaze', (state, e) => {
 
 function NewUser() {
   return (
-    <main className="app-container" style={{ justifyContent: 'center' }}>
-      <form onSubmit={registerUser} style={{ margin: 'auto' }}>
+    <main className="app-container">
+      <img src={logo} alt="Blaze" class="brand" />
+
+      <img
+        class="network-img"
+        src={network}
+        alt="Devices connected using Blaze"
+      />
+
+      <div class="register-info">
+        <h1>Choose a nickname</h1>
+        <p>
+          Nicknames are used to identify different devices in a common file
+          sharing room. A room must always have devices with unique nicknames.
+        </p>
+      </div>
+
+      <form onSubmit={registerUser} class="register-form">
         <NicknameInput input={{ style: { marginBottom: 40 } }} />
         <button type="submit" class="btn wide">
           Continue
