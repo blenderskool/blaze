3232d6fb80bdb139487a4a4acd96c71a57be374f
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2022-12-25 08:08:42 +0000

    Fix typo

--- client/src/routes/App/Rooms/components/LocalRoomHelpModal/LocalRoomHelpModal.js
+++ client/src/routes/App/Rooms/components/LocalRoomHelpModal/LocalRoomHelpModal.js
@@ -4,18 +4,20 @@ import PropTypes from 'prop-types';
 import Modal from '../../../../../components/Modal/Modal';
 import './LocalRoomHelpModal.scoped.scss';
 
-function LocalRoomHelpModal({ onRoomJoin, ...props}) {
+function LocalRoomHelpModal({ onRoomJoin, ...props }) {
   return (
     <Modal {...props}>
       <div class="local-room-help">
         <h2>Local Network room</h2>
         <p>
-          Local network room creates a private and isolated room among devices in the same network connection sharing the same public IP address.
+          Local network room creates a private and isolated room among devices
+          in the same network connection sharing the same public IP address.
           Devices outside the local network cannot join this room.
           <br />
           <br />
-          If Blaze is unable to detect devices in your local network, you can still create a publich file sharing room
-          that allows any device connected to the internet to join the room.
+          If Blaze is unable to detect devices in your local network, you can
+          still create a public file sharing room that allows any device
+          connected to the internet to join the room.
         </p>
         <button class="btn wide" onClick={() => onRoomJoin('')}>
           Join local room
