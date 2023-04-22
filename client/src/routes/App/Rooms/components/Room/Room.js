79bacc6fd4a29d2a864d3daead104082dacba52c
David Alfonzo P <46352804+Dalfonzo@users.noreply.github.com>
2023-04-22 16:10:59 -0400

    Fix rendering of emojis as first character in nickname (#167)
    * Fix rendering of emojis as first character in nickname
    
    * Adds util function that extracts the first character from a string
    
    * Adds the new util function to the places where the first character from a string is required
    
    * Update client/src/utils/getFirstCharacterFromString.js
    
    Updates function return description
    
    Co-authored-by: Akash Hamirwasia <akash.hamirwasia@gmail.com>
    
    * Update client/src/utils/getFirstCharacterFromString.js
    
    Deletes extra line
    
    Co-authored-by: Akash Hamirwasia <akash.hamirwasia@gmail.com>
    
    ---------
    
    Co-authored-by: Akash Hamirwasia <akash.hamirwasia@gmail.com>

--- client/src/routes/App/Rooms/components/Room/Room.js
+++ client/src/routes/App/Rooms/components/Room/Room.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { X } from 'preact-feather';
 
+import getFirstCharacterFromString from '../../../../../utils/getFirstCharacterFromString';
+
 import './Room.scoped.scss';
 
 function RoomContainer({ as, className, highlighted, ...props }) {
@@ -82,7 +84,7 @@ RoomSecondaryAction.defaultProps = {
 function RoomPeers({ className, localPeers, ...props }) {
   return (
     <ul className={cx('peers secondary-action', className)} {...props}>
-      {localPeers.slice(0, 3).map((peer) => <li class="peer" title={peer.name}>{peer.name[0]}</li>)}
+      {localPeers.slice(0, 3).map((peer) => <li class="peer" title={peer.name}>{getFirstCharacterFromString(peer.name)}</li>)}
       {localPeers.length > 3 && (
         <li class="extra">
           +
---
+++ client/src/utils/getFirstCharacterFromString.js
@@ -0,0 +1,8 @@
+/**
+ * Returns the first character from a string preserving emoji characters
+ * @param {String} str Text from which the first character will be extracted
+ * @returns {String} First character from the string provided
+ */
+export default function getFirstCharacterFromString(str) {
+  return String.fromCodePoint(str.codePointAt(0));
+}
