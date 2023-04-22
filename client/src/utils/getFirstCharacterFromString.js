a75d5c03ebb580f3b29babd55b9bc5a22dd682bc
David Pérez Malavé <dalfonzo18@gmail.com>
2023-04-22 15:19:42 -0400

    Adds util function that extracts the first character from a string

---
+++ client/src/utils/getFirstCharacterFromString.js
@@ -0,0 +1,9 @@
+/**
+ * Returns the first character from a string
+ * @param {String} str Text from which the first character will be extracted
+ * @returns {String} First character from the string provided
+ */
+
+export default function getFirstCharacterFromString(str) {
+  return String.fromCodePoint(str.codePointAt(0));
+}
